9b18c196ec17941415237ce56b2c0ea044734d7a
jisu <1712wltn@gmail.com>
2022-08-19 12:26:32 +0900

    0819 섹션 7.4 롬복 라이브러리 적용

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -13,7 +13,6 @@ public class OrderServiceImpl implements OrderService{
     private final DiscountPolicy discountPolicy;
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 
-    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
